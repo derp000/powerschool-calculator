ddab8c41c9aa3b4045076f8d5083b7dbf35260ea
derp000 <84947701+derp000@users.noreply.github.com>
2022-06-30 22:40:30 -0400

    Add print optional functionality

--- python/ps_calc.py
+++ python/ps_calc.py
@@ -48,7 +48,7 @@ def get_data(
         raise AttributeError("get_data() likely failed and returned None")
 
 def make_csv(raw_grades: list, mp_amt: int, *missing_grades) -> None:
-        with open("python/temp.csv", "w", encoding="UTF8", newline="") as fp:
+        with open("temp.csv", "w", encoding="UTF8", newline="") as fp:
             writer = csv.writer(fp)
             writer.writerow(["MP1", "MP2", "MP3", "MP4"])
             
@@ -70,9 +70,9 @@ def make_csv(raw_grades: list, mp_amt: int, *missing_grades) -> None:
             writer.writerows(classes)
 
 def load_df() -> pd.DataFrame:
-    return pd.read_csv("python/temp.csv")
+    return pd.read_csv("temp.csv")
 
-def calc_qpa(df: pd.DataFrame, sci_period=[0], missing=0) -> float:
+def calc_qpa(df: pd.DataFrame, sci_period=[0], missing=0, fmt_print=False) -> float:
     credit = (len(df)-missing) * 1.25
     for _ in sci_period:
         credit += 0.25
@@ -82,5 +82,6 @@ def calc_qpa(df: pd.DataFrame, sci_period=[0], missing=0) -> float:
         df.loc[p] *= 1.2
 
     qpa = df.sum()/credit
-    print(f"{df.name} QPA: {round(qpa, 2)}")
+    if fmt_print:
+        print(f"{df.name} QPA: {round(qpa, 2)}")
     return qpa
